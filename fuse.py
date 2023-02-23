e2d45e5d8a3736c9ba985a96bb1182442fde983d
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2023-02-23 13:41:53 +1000

    switching from minidom to etree xml parser

--- fuse.py
+++ fuse.py
@@ -4,62 +4,96 @@ import html
 import json
 import os
 import sys
-from xml.dom import minidom
+from xml.etree import ElementTree
 
 # parsing xml & json can be very unsafe with a lazy parser, always verify schema before diving in!
 if not sys.argv != (sys.argv[0], "--unsafe"):
     unsafe = False
-    import xmlschema  # noqa F401
-    import jsonschema  # noqa F401
-    # TODO: pre-load schemas from tests/schema
+    # from tests import test_json, test_xml
 else:
     unsafe = True
     # TODO: UserWarning
 
 
-ent_filename = {"ENTITIES.xml": "entities.ent",
-                "ENTITIES_env.xml": "environment_entities.ent",
-                "ENTITIES_fx.xml": "effects_entities.ent",
+ent_filename = {"ENTITIES.xml":        "entities.ent",
+                "ENTITIES_env.xml":    "environment_entities.ent",
+                "ENTITIES_fx.xml":     "effects_entities.ent",
                 "ENTITIES_script.xml": "script_entities.ent",
-                "ENTITIES_spawn.xml": "spawn_entities.ent",
-                "ENTITIES_snd.xml": "sound_entities.ent"}
+                "ENTITIES_spawn.xml":  "spawn_entities.ent",
+                "ENTITIES_snd.xml":    "sound_entities.ent"}
 
 key_types = {"real": float, "integer": int, "string": str, "boolean": bool,
-             "angles": str, "model": str, "color": str,  # TODO: regex
+             "angles": str, "model": str, "color": str,
              "targetname": str}  # TODO: see if I forgot any
-# NOTE: origin & any *keys (added by compiler) should not appear in definitions
-# -- we might override origin in future actually... some ents might need deeper descriptions
+
 key_type_defaults = {"float": "1.0", "integer": "0", "string": "", "boolean": "0",
                      "angles": "0 0 0", "model": "", "color": "1 0 1", "targetname": ""}
-# NOTE: choiceTypes should default to first Option
+# TODO: use regex to ensure values conform to key_type
+# NOTE: origin & any *keys (added by compiler) should not appear in definitions
+# -- we might override origin in future actually... some ents might need deeper descriptions
+
+# TODO: choiceTypes should default to first list item
 # -- any type not in key_types should be considered a choiceType
+# -- throw a Warning if choiceType is not defined and default to string
 
 
-def override_header(xml_ent: minidom.Element, json_ent: dict, attr: str, default=None):
-    value = xml_ent.getAttribute(attr.lower())  # Color -> color
-    if value == "":
+def update_ent_metadata(xml_ent: ElementTree.Element, json_spec: dict, attr: str, default=None):
+    """Sets one attribute at a time; handles type conversions & default heirarchy"""
+    value = xml_ent.get(attr.lower())  # Color -> color
+    if value is None:
         value = default
-    value = json_ent.get(attr, value)
+    value = json_spec.get(attr, value)
     if value is not None:
-        xml_ent.setAttribute(attr.lower(), value)
+        xml_ent.set(attr.lower(), value)
 
 
-def override_key(xml_key: minidom.Element, json_key: dict):
-    xml_key.tagName = json_key.get("type", xml_key.tagName)
-    for attr in ("name", "value"):
-        xml_key.setAttribute(attr, json_key.get(attr, xml_key.getAttribute(attr)))
-        # NOTE: will be set to "" if not present in xml_key
-    xml_desc = xml_key.childNodes[0].data  # assuming this is always present in mrvn/*.xml
-    xml_key.childNodes = [minidom.Text()]
-    xml_key.childNodes[0].replaceWholeText(json_key.get("description", xml_desc))
+def new_key(json_spec: dict) -> ElementTree.Element:
+    """Not the same as: e = Element('x'); update_key(e, json_spec)"""
+    out = ElementTree.Element(json_spec["type"])
+    out.set("key", json_spec["keyname"])  # compiled name
+    out.set("name", json_spec.get("name", json_spec["keyname"]))  # editor friendly name
+    # TODO: key_type_defaults?, choiceType defaults need a Dict[str, choiceType_spec] arg
+    out.set("value", json_spec.get("value", ""))  # default value
+    out.text = html.escape(json_spec.get("description", ""))  # use / effects
+    return out
 
 
-# TODO: new_key, override_spawnflag & new_spawnflag functions
+def update_key(xml_key: ElementTree.Element, json_spec: dict):
+    xml_key.tag = json_spec.get("type", xml_key.tag)
+    for attr in ("name", "keyname", "default", "value"):
+        if json_spec.get(attr) is None and xml_key.get(attr) is None:
+            continue
+        xml_key.set(attr, json_spec.get(attr, xml_key.get(attr)))
+    # TODO: UX DESIGN QUESTION: do we expect pilot/*.json to html.escape() descriptions?
+    xml_key.text = json_spec.get("description", xml_key.text)
+
+
+def new_spawnflag(json_spec: dict) -> ElementTree.Element:
+    out = ElementTree.Element("flag")
+    out.set("key", json_spec["name"])
+    out.set("name", json_spec["name"])
+    out.set("bit", json_spec["bit"])  # bit set / unset by flag
+    default = {"False": "0", "True": "1"}[json_spec.get("default", "False")]
+    out.set("value", str(default))
+    out.text = html.escape(json_spec.get("description", ""))  # use / effects
+    return out
+
+
+def update_spawnflag(xml_flag: ElementTree.Element, json_spec: dict):
+    keyname = json_spec.get("name", xml_flag.get("key"))
+    xml_flag.set("key", keyname)
+    xml_flag.set("name", keyname)
+    xml_flag.set("value", json_spec.get("value", xml_flag.get("value")))
+    # TODO: UX DESIGN QUESTION: do we expect pilot/*.json to html.escape() descriptions?
+    xml_flag.text = json_spec.get("description", xml_flag.text)
 
 
 if __name__ == "__main__":
     # TODO: make failstates more explicit and give useful error messages
-    # -- ideally gather all errors & report changes needed to input
+    # -- ideally gather all errors & recommend changes to pilot/*.json (mrvn/*.xml should be stable)
+    # NOTE: we use .__getitem__ rather than .get in cases where missing keys should fail
+    # -- this doesn't tell a casual user what they forgot to add; we need to rectify that.
+    # TODO: pre-process input files w/ schema before running fuse; leave input validation up to tests
     for game in filter(lambda p: os.path.isdir(os.path.join("mrvn", p)), os.listdir("mrvn")):
         print(f"processing mrvn/{game}/")
         in_dir = os.path.join("mrvn", game)
@@ -69,99 +103,66 @@ if __name__ == "__main__":
         print(f"gathering overrides from pilot/{game}/")
         json_dir = os.path.join("pilot", game)
         assert os.path.exists(json_dir)
-
         ent_overrides = collections.defaultdict(set)
         # ^ {"ENTITIES.xml": {"entity.json"}}
         for json_filename in fnmatch.filter(os.listdir(json_dir), "*.json"):
+            full_json_filepath = os.path.join(json_dir, json_filename)
+            # if not unsafe:
+            #     test_json.test_entity(full_json_filepath)
             try:
-                with open(os.path.join(json_dir, json_filename)) as json_file:
+                with open(full_json_filepath) as json_file:
                     entity = json.load(json_file)
             except Exception as exc:
-                print(f"{json_filename} broke ({exc.__name__})")
-                # raise exc
-            # TODO:
-            # if safe:  # copy of tests.test_json.validate()
-            #     assert jsonschema.is_valid(entity)
-            #     assert entity["Entity"] in blocks[entity["Block"]]
-            #     # TODO: validate type defaults
+                print(f"{json_filename} broke: {exc.__name__}({exc.msg})")  # TODO: more detail / helpful suggestions
             ent_overrides[f"{entity['Block']}.xml"].add(json_filename)
         print(f"collected {sum(map(len, ent_overrides.values()))} entities across {len(ent_overrides)} blocks")
 
-        # utility node
-        newline = minidom.Text()  # Can't initialise w/ a value?
-        newline.replaceWholeText("\n")
+        # TODO: find all defined choiceTypes (in mrvn/*.xml & pilot/*/choiceTypes/*.json)
 
         for xml_filename in fnmatch.filter(os.listdir(in_dir), "*.xml"):
             print(f"processing mrvn/{game}/{xml_filename}")
             full_xml_filename = os.path.join(in_dir, xml_filename)
             assert xml_filename in ent_filename.keys(), "Unexpected .xml file in mrvn/"
-            # NOTE: validate w/ pytest first! not secure against malicious data!
-            # TODO:
-            # if not unsafe:
-            #     xml_ent_schema.validate(full_xml_filename)
-            xml_file = minidom.parse(full_xml_filename)
-            classes_node = xml_file.getElementsByTagName("classes")[0]
-            elements = tuple(filter(lambda x: isinstance(x, minidom.Element), classes_node.childNodes))
-            # choiceTypes = {e.attributes["name"].value: e for e in elements if e.tagName == "list"}
-            # ^ choiceTypes present in mrvn .xml
-            choiceTypes = set()  # choiceTypes to add
-            entities = {e.attributes["name"].value: e for e in elements if e.tagName in ("point", "group")}
+            xml_file = ElementTree.parse(full_xml_filename)
+            ent_classes_node = xml_file.getroot()
+            entities = {e.get("name"): e for e in ent_classes_node if e.tag in ("point", "group")}
             for json_filename in ent_overrides[xml_filename]:
                 with open(os.path.join(json_dir, json_filename)) as json_file:
                     json_ent = json.load(json_file)
                 # index entity
-                if json_ent["Entity"] not in entities:  # editorclass?
-                    # see https://github.com/MRVN-Radiant/MRVN-Radiant/issues/26
-                    print("Couldn't find", json_ent["Entity"], "to override (editorclass?). Skipping...")
+                if json_ent["Entity"] not in entities:
+                    # TODO: add new entities (e.g. prop_static)
+                    print("Couldn't find", json_ent["Entity"], f"to override in {full_xml_filename}; Skipping...")
                     continue
                 xml_ent = entities[json_ent["Entity"]]
-                # credit contributors
-                contributors_comment = minidom.Comment(", ".join(json_ent["Contributors"]))
-                classes_node.childNodes.insert(classes_node.childNodes.index(xml_ent), contributors_comment)
-                classes_node.childNodes.insert(classes_node.childNodes.index(xml_ent), newline)
-                # override entity head node attributes
-                override_header(xml_ent, json_ent, "Color", default="1 0 1")
-                if xml_ent.tagName == "point":
-                    override_header(xml_ent, json_ent, "Box", default="-8 -8 -8 8 8 8")
-                override_header(xml_ent, json_ent, "Model")
-                # override entity keys
-                xml_keys = {k.attributes["key"].value: k for k in xml_ent.childNodes
-                            if k.attributes is not None and "key" in k.attributes}
+                contributors_comment = ElementTree.Comment(", ".join(json_ent["Contributors"]))
+                ent_classes_node.insert(ent_classes_node[::].index(xml_ent), contributors_comment)
+                update_ent_metadata(xml_ent, json_ent, "Color", default="1 0 1")
+                if xml_ent.tag == "point":
+                    update_ent_metadata(xml_ent, json_ent, "Box", default="-8 -8 -8 8 8 8")
+                update_ent_metadata(xml_ent, json_ent, "Model")
+                xml_keys = {k.get("key"): k for k in xml_ent if k.tag != "flag"}
                 json_keys = {k["keyname"]: k for k in json_ent.get("Keys", dict())}
-                choiceTypes.update({t for t in {k.get("type", "string") for k in json_keys.values()}
-                                    if t not in key_types})
+                # TODO: add new choiceTypes to .ent
+                # update xml ent according to json spec
                 for keyname in set(xml_keys).intersection(set(json_keys)):
-                    override_key(xml_keys[keyname], json_keys[keyname])
-                # write new keys
-                key_ends = [" ".join(["-" * 5, x, "-" * 5]) for x in ("SPAWNFLAGS", "NOTES")]
-                # TODO: find a better way to insert keys in the right place
-                # TODO: sort keys?
-                last_key_index = min([i for i, e in enumerate(xml_ent.childNodes)
-                                      if isinstance(e, minidom.Text) and any([t in e.data for t in key_ends])])
-                for keyname in set(json_keys).difference(set(xml_keys)):  # new keys
-                    json_key = json_keys[keyname]
-                    new_key = xml_file.createElement(json_key["type"])
-                    new_key.setAttribute("keyname", keyname)
-                    new_key.setAttribute("name", json_key.get("name", keyname))
-                    new_key.setAttribute("value", json_key.get("value", ""))  # default
-                    new_key.childNodes = [minidom.Text()]
-                    # TODO: default to previous description
-                    json_desc = json_key.get("description", "")
-                    new_key.childNodes[0].replaceWholeText(html.escape(json_desc))
-                    xml_ent.childNodes.insert(last_key_index, new_key)
+                    update_key(xml_keys[keyname], json_keys[keyname])
+                # TODO: cut off the tail & regrow, rather than mutating in place
+                last_key_index = len(xml_ent)
+                xml_spawnflags = [k for k in xml_ent if k.tag == "flag"]
+                if len(xml_spawnflags) > 0:
+                    last_key_index = xml_ent[::].index(xml_spawnflags[0]) - 1
+                # TODO: ensure "----- SPAWNFLAGS -----" spacer is positioned correctly
+                for keyname in set(json_keys).difference(set(xml_keys)):
                     last_key_index += 1
+                    xml_ent.insert(last_key_index, new_key(json_keys[keyname]))
+                xml_spawnflags = {int(f.get("bit")): f for f in xml_spawnflags}
+                json_spawnflags = {int(f.get("bit")): f for f in json_ent.get("SpawnFlags", list())}
+                for i in range(32):  # sorting
+                    if i not in {*xml_spawnflags, *json_spawnflags}:
+                        continue
                 ...
-                # TODO: override SpawnFlags
-                # -- add ----- SPAWNFLAGS ----- line if absent
-                # -- key
-                # -- name
-                # -- default
-                # -- description
                 # TODO: override Notes (preserve "Introduced by Source / Titanfall" on first line)
-            # TODO: add choiceTypes
-            # write changes
             with open(os.path.join(out_dir, ent_filename[xml_filename]), "w") as ent_file:
-                xml_file.writexml(ent_file)
-            # TODO: header comments get a little mangled
-            # -- might need to clean up post-write?
-        # TODO: check for unused choiceTypes
+                xml_file.write(ent_file)
+        # TODO: check for unused choiceTypes & log warnings
