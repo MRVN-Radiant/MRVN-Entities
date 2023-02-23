3247c370bff5cb0fe5b8385161772011912e59d5
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2023-02-23 13:35:33 +1000

    planning out more json tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,6 +1,7 @@
 import fnmatch
 import json
 import os
+import re
 
 import jsonschema
 import pytest
@@ -22,14 +23,46 @@ for game_dir in games:
         choiceType_json.append(os.path.join(search_dir, choiceType).replace("\\", "/"))
 
 
+def valid_conversion(value: str, func, count: int) -> bool:
+    try:
+        test = [float(a) for a in value.split()]
+    except ValueError:
+        return False
+    return len(test) == count
+
+
+# TODO: verify keyvalues
+# https://github.com/MRVN-Radiant/MRVN-Radiant/blob/main/radiant/eclass_xml.cpp#L49-L68
+key_tests = {"string": r".*",
+             "array": r"[^;]*|([^;];)*[^;]+",
+             "integer": lambda v: valid_conversion(v, int, 1),
+             "boolean": r"0|1",
+             # floats
+             "real": lambda v: valid_conversion(v, float, 1),
+             "angle": lambda v: valid_conversion(v, float, 3),
+             "direction": lambda v: valid_conversion(v, float, 3),  # + range check?
+             "real3": lambda v: valid_conversion(v, float, 3),
+             "angles": lambda v: valid_conversion(v, float, 3),  # + range check?
+             "color": lambda v: valid_conversion(v, float, 3),  # + range check?
+             # identifiers
+             "target": r".*",
+             "targetname": r".*",
+             # paths
+             "sound": r".*",
+             "texture": r".*",
+             "model": r".*",
+             "skin": r".*"}
+# ^ {"keyName": test_func or r"test regex"}
+key_tests = {k: t if not isinstance(t, str) else (lambda v: re.match(t, v) is not None) for k, t in key_tests.items()}
+
+
 @pytest.mark.parametrize("json_filename", entity_json)
 def test_entity(json_filename: str):
     with open(json_filename) as json_file:
         entity = json.load(json_file)
     jsonschema.validate(entity, schema=entity_schema)
-    # TODO: dynamic type checking
-    # -- list of valid key types for radiant
-    # -- add choiceType lists from .xml & .json
+    # TODO: test choiceTypes are defined in either mrvn/*.xml or pilot/*/choiceTypes/*.json
+    # TODO: verify any choiceType selections are valid
 
 
 @pytest.mark.parametrize("json_filename", choiceType_json)
@@ -37,6 +70,7 @@ def test_choiceType(json_filename: str):
     with open(json_filename) as json_file:
         choiceType = json.load(json_file)
     jsonschema.validate(choiceType, schema=choiceType_schema)
+    # TODO: enforce filename matches
 
 
 blocks = json.load(open("blocks.json"))
